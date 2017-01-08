a9bd27ad717c75d0af73f94e3b1003ddfbaf8140
Muhammad Moaz <m-moaz@hotmail.com>
2017-01-09 02:26:18 +0500

    Initial Commit

---
+++ FRP Login/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FRP Login
+//
+//  Created by Muhammad Moaz on 1/9/17.
+//  Copyright © 2017 Muhammad Moaz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
